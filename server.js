d562a0767b16707911d3dbe971b8491253796c01
snovosel <novosel991@gmail.com>
2019-03-10 11:28:28 -0400

    functional with rooms

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
 var app = require("express")();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
-var cors = require('cors');
+var cors = require("cors");
 
 app.use(cors());
 
 // socket connection
-io.on('connection', socket => {
+io.on("connection", socket => {
   // broadcast any message sent through the socket
-  socket.on('message', data => {
-    console.log('message received', data);
-    io.emit('message', data);
-  });
+  socket.on("message", ({ room, ...restData }) =>
+    io.sockets.in(room).emit("message", restData)
+  );
 
   // join a room
-  socket.on('room', room => {
-    console.log('joining room....' + room);
+  socket.on("room", room => {
+    console.log("joining room...." + room);
     socket.join(room);
   });
 });
