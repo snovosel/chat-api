96f32d94edc02ea3578e72d8eb67469870126008
Steve Novosel <novosel991@gmail.com>
2019-03-16 19:38:32 -0400

    removed uneccessary comments

--- server.js
+++ server.js
@@ -7,24 +7,13 @@ app.use(cors());
 
 let lastFiveMessages = [];
 
-// socket connection
 io.on("connection", socket => {
-  // broadcast any message sent through the socket
   socket.on("message", ({ room, ...restData }) => {
-    if (lastFiveMessages.length === 4) {
-      lastFiveMessages = [];
-    }
-
-    lastFiveMessages.push(restData);
-    console.log("ON MESSAGE", lastFiveMessages);
     io.sockets.in(room).emit("message", restData);
   });
 
-  // join a room
   socket.on("room", room => {
     socket.join(room);
-    // console.log("ON JOIN", lastFiveMessages);
-    // socket.emit("join", lastFiveMessages);
     socket.emit("join");
   });
 
