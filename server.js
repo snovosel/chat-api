1c4a96aa1a5d33366af09e040d137b37169e0597
Steve Novosel <novosel991@gmail.com>
2019-03-17 12:26:44 -0400

    connecting web socket to db

--- server.js
+++ server.js
@@ -1,20 +1,61 @@
 var app = require("express")();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
-var cors = require("cors");
+var mongoose = require("mongoose");
 
-app.use(cors());
+// db connection -------------------------------------------------------
 
-let lastFiveMessages = [];
+mongoose.connect(
+  "mongodb+srv://snovosel:Flwr1281!@psst-ombxx.mongodb.net/test?retryWrites=true",
+  { useNewUrlParser: true },
+  err => {
+    if (err) {
+      console.log("Some problem with the connection " + err);
+    } else {
+      console.log("The Mongoose connection is ready");
+    }
+  }
+);
+
+//Get the default connection
+let db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+// models -----------------------------------------------------
+
+//Require Mongoose
+var mongoose = require("mongoose");
+
+//Define a schema
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+  user_name: { type: String, required: true, max: 100 }
+});
+
+const User = mongoose.model("User", UserSchema);
+
+// ------ web socket ------------------------
 
 io.on("connection", socket => {
-  socket.on("message", ({ room, ...restData }) => {
-    io.sockets.in(room).emit("message", restData);
-  });
+  socket.on("room", ({ room, name }) => {
+    let user = new User({ user_name: name });
+    const findUser = async function(params) {
+      try {
+        return await User.find(params);
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
-  socket.on("room", room => {
+    const users = findUser();
+    console.log("users", users);
     socket.join(room);
     socket.emit("join");
+  });
+
+  socket.on("message", ({ room, ...restData }) => {
+    io.sockets.in(room).emit("message", restData);
   });
 
   socket.on("leave", room => {
